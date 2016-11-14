b498f762441964f6d2dcc6eb65c92856e7708cdc
alvarogomezmu <alvarog.m@hotmail.com>
2016-11-14 19:25:51 +0100

    xPath

---
+++ AD/Tema3/src/primerosPasos/leerXML.java
@@ -0,0 +1,35 @@
+package primerosPasos;
+
+import java.io.*;
+import java.util.*;
+import org.jdom2.*;
+import org.jdom2.filter.Filters;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+
+/**
+ *
+ * @author AlumnoT
+ */
+public class leerXML {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws JDOMException, IOException {
+        // LEER
+        SAXBuilder b = new SAXBuilder();
+        Document doc = b.build(new FileInputStream("archivoLeer.xml"));
+
+        // CONSULTAR
+        XPathExpression<Element> xpath = XPathFactory.instance().compile("expresion xpath", Filters.element());
+        List<Element> elemento = xpath.evaluate(doc);
+        Iterator it = elemento.iterator();
+
+        while (it.hasNext()) {
+            Element at = (Element) it.next();
+            System.out.println(at.getName() + ": " + at.getText());
+        }
+    }
+}
