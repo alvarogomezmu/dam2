0f9c82d4d756929ea17ecaf7eb400fcdd1b2f83a
alvarogomezmu <alvarog.m@hotmail.com>
2017-01-17 19:18:02 +0100

    sqlite pruebas

--- AD/segundaEvaluacion/2evaluacion/src/sqlite/sqlite.java
+++ AD/segundaEvaluacion/2evaluacion/src/sqlite/sqlite.java
@@ -3,6 +3,7 @@ package sqlite;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -25,6 +26,34 @@ public class sqlite {
             System.exit(0);
         }
         System.out.println("Abierta la BBDD");
+        
+        createNewTable();
+        insertarDatos();
+
     }
 
+    /**
+     * Create a new table in the test database
+     *
+     */
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C:\\sqlite3\\curso.db";
+
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS ejemplo (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	nom text NOT NULL,\n"
+                + "	sueldo integer\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
