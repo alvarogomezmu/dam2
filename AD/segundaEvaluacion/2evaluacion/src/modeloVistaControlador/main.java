b7fe25541df258df7178172099c7862773fa6c5d
alvarogomezmu <alvarog.m@hotmail.com>
2017-01-25 15:56:42 +0100

    25-1-2017

--- AD/segundaEvaluacion/2evaluacion/src/modeloVistaControlador/main.java
+++ AD/segundaEvaluacion/2evaluacion/src/modeloVistaControlador/main.java
@@ -1,12 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modeloVistaControlador;
-
-import static java.awt.PageAttributes.MediaType.C;
-import static javafx.scene.input.KeyCode.V;
 
 /**
  *
@@ -19,10 +11,9 @@ public class main {
      */
     public static void main(String[] args) {
         new BBDD("test.db"); // llama a BBDD para conectar
-	Vista v = new Vista(); //instancia ventana
+        Vista v = new Vista(); //instancia ventana
         Controlador c = new Controlador(v); // conecta la vista al controlador
         v.conectarControlador(c); // realiza la conexion + vista + controlador
-        
-    }
 
+    }
 }
