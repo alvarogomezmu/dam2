9bba369680158d5fee4c518af84035aa85118475
alvarogomezmu <alvarog.m@hotmail.com>
2017-03-07 19:24:04 +0100

    repaso1

--- AD/segundaEvaluacion/2evaluacion/src/OracleRepaso1/Main.java
+++ AD/segundaEvaluacion/2evaluacion/src/OracleRepaso1/Main.java
@@ -14,12 +14,18 @@ public class Main {
      */
     public static void main(String[] args) {
         try {
-            Connection dbConnection;
-            dbConnection = Herramientas.connectORACLE("usu1", "root");
+
+            Herramientas.connectORACLE("usu1", "root");
             System.out.println("1. Debemos crear un procedimiento o función que  muestre el  nombre del tema, "
                     + "y la categoría del libro  teniendo en cuenta que la categoría del libro  nos interesa que "
                     + "tengan 2  “es” y que la longitud  del nombre del tema tiene que ser mayor o igual a 7 caracteres");
-            Metodos.callOracleStoredProcOUTParameter(dbConnection);
+            Metodos.ApartadoUno();
+            // -----
+            System.out.println("2. Vamos a crear un procedimiento  o una  función que pasándole el tema Aventura "
+                    + "por parámetro, nos debe mostrar los autores que han escrito 2 o más libros sobre ese tema");
+            Metodos.ApartadoDos();
+            // -----
+            
 
         } catch (SQLException e) {
 
--- AD/segundaEvaluacion/2evaluacion/src/OracleRepaso1/Metodos.java
+++ AD/segundaEvaluacion/2evaluacion/src/OracleRepaso1/Metodos.java
@@ -1,6 +1,7 @@
 package OracleRepaso1;
 
 import java.sql.*;
+import Herramientas.*;
 
 /**
  *
@@ -8,13 +9,16 @@ import java.sql.*;
  */
 public class Metodos {
 
-    public static void callOracleStoredProcOUTParameter(Connection dbConnection) throws SQLException {
+    // Method before been renamed: callOracleStoredProcOUTParameter
+    public static void ApartadoUno() throws SQLException {
 
+        Connection dbConnection = null;
         CallableStatement callableStatement = null;
 
         String getDBUSERByUserIdSql = "{call CATEGORIA_ES(?,?)}";
 
         try {
+            dbConnection = Herramientas.connectORACLE("usu1", "root");
             callableStatement = dbConnection.prepareCall(getDBUSERByUserIdSql);
 
             callableStatement.registerOutParameter(1, java.sql.Types.VARCHAR);
@@ -43,4 +47,81 @@ public class Metodos {
             }
         }
     }
+
+//    public static void ApartadoDoss() throws SQLException {
+//
+//        CallableStatement callableStatement = null;
+//        Connection dbConnection = null;
+//
+//        String getDBUSERByUserIdSql = "{call AUTORESDOSLIBROS(?)}";
+//
+//        try {
+//            dbConnection = Herramientas.connectORACLE("usu1", "root");
+//            callableStatement = dbConnection.prepareCall(getDBUSERByUserIdSql);
+//
+//            callableStatement.setString(1, "Aventura");
+//            callableStatement.registerOutParameter(1, java.sql.Types.VARCHAR);
+//
+//            callableStatement.execute();
+//            
+//            final ResultSet rs = callableStatement.getResultSet();
+//
+//            while (rs.next()) {  
+//                 System.out.println("Autores que han escrito 2 o mas libros sobre el tema 'Aventura':"+rs.getString("inputParam"));  
+//             }
+//            String nombre = callableStatement.getString(1);  
+//            //System.out.println("Parametro de salida incrementado="+nombre);
+//            //String nombre = callableStatement.getString(1);
+//
+//            System.out.println(" " + nombre);
+//
+//        } catch (SQLException e) {
+//
+//            System.out.println(e.getMessage());
+//
+//        } finally {
+//
+//            if (callableStatement != null) {
+//                callableStatement.close();
+//            }
+//
+//            if (dbConnection != null) {
+//                dbConnection.close();
+//            }
+//        }
+//    }
+    public static void ApartadoDos() throws SQLException {
+
+        Connection conn = null;
+
+        try {
+            // Creamos la conexion  
+            conn = Herramientas.connectORACLE("usu1", "root");
+            conn.setAutoCommit(false);
+
+            String nombre="";
+            CallableStatement cs = conn.prepareCall("{?=call AUTORESDOSLIBROS(?)}");
+            cs.registerOutParameter(1, java.sql.Types.VARCHAR);
+            cs.setString(2, "Aventura");
+            cs.executeUpdate();
+            nombre=cs.getString(1);
+            System.out.println(nombre);
+//            
+//            cStmt.setString(1, "Aventura");
+//            cStmt.registerOutParameter(1, java.sql.Types.VARCHAR);
+//
+//             cStmt.executeUpdate();    
+//   
+//            System.out.println("Cadena de caracteres pasada como parametro de entrada="+cStmt.getString(1));  
+// 
+//               
+//             String outputValue = cStmt.getString(1);  
+//             System.out.println("Parametro de salida incrementado="+outputValue);   
+        }catch (Exception e) {  
+            conn.rollback();  
+            e.printStackTrace();  
+        }finally{  
+            conn.close();  
+        }  
+    }  
 }
